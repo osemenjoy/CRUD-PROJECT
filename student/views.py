c143040ca14560075bd739af47c2d0d53f472f45
osemenjoy <osemenjoy448@gmail.com>
2024-06-20 21:38:26 +0100

    list templates added

--- student/views.py
+++ student/views.py
@@ -6,13 +6,4 @@ from django.urls import reverse_lazy
 class StudentListView(ListView):
     model = Student
     template_name = "student-list.html"
-
-
-class StudentUpdateView(UpdateView):
-    model = Student
-    template_name = "student-update.html"
-
-class StudentDeleteView(DeleteView):
-    model = Student
-    template_name = "student-delete.html"
-    success_url = reverse_lazy("student-list")
+    context_object_name = "students"
