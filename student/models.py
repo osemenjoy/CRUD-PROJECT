355ae5a0c8d5e31c3121ae0927f458d33dbf234c
osemenjoy <osemenjoy448@gmail.com>
2024-06-20 21:13:05 +0100

    Database created

--- student/models.py
+++ student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first = models.CharField(max_length=200, blank=True, null=True)
+    last = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length= 20)
+    location = models.CharField(max_length=200)
+    hobby = models.CharField(max_length=200)
+    course = models.CharField(max_length= 200, default= "computer science")
+
+
+    def __str__(self):
+        return self.first
