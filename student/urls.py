11eeddf3619f429b4b73cf072cc4df733ac36850
osemenjoy <osemenjoy448@gmail.com>
2024-06-20 21:55:44 +0100

    edit url added

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,5 @@ from .views import StudentListView, StudentUpdateView
 
 urlpatterns = [
     path("",StudentListView.as_view(), name = "student-list" ),
- 
+    path("edit/<int:pk>", StudentUpdateView.as_view(), name = "student-update"),
 ]
