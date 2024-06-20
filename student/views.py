dc898780a65ba80dd90ca379383436729e845210
osemenjoy <osemenjoy448@gmail.com>
2024-06-20 21:41:31 +0100

    update view added

--- student/views.py
+++ student/views.py
@@ -7,3 +7,8 @@ class StudentListView(ListView):
     model = Student
     template_name = "student-list.html"
     context_object_name = "students"
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = "student-update.html"
